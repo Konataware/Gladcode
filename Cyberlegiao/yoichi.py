82829befc11b3c2ce31c6191cb40f3ef858a4576
Daniel Pansera <140283213+DanielPansera@users.noreply.github.com>
2023-10-10 15:12:51 -0300

    Update yoichi.py
    Codigo do Yoichi!

--- Cyberlegiao/yoichi.py
+++ Cyberlegiao/yoichi.py
@@ -1,1 +1,39 @@
+import random
+start = True
+Agilidade = 0
+Força = 0
+Inteligente = 0
+randomico = random.randint(1, 12)
+SegundoRandomico = random.randint(1, 12)
+
+def loop():
+    global start, Agilidade, Força, Inteligente, randomico, SegundoRandomico
+    Agilidade = getAGI()
+    Força = getSTR()
+    Inteligente = getINT()
+    if Agilidade < 25:
+        upgradeAGI(5)
+    elif Inteligente <= 20 and Agilidade >= 25:
+        upgradeINT(5)
+    if not isSafeHere():
+        moveTo(12.5, 12.5)
+    elif getHit() and doYouSeeMe():
+        turnToLastHit()
+        stepLeft()
+    elif getCloseEnemy():
+      if getDistToTarget() < 4:
+        stepBack()
+        attackRanged(getTargetX(), getTargetX())
+      if getAmbushTimeLeft() <= 1:
+            ambush()
+            if isStunned() and not doYouSeeMe():
+                attackRanged(getTargetX(), getTargetY())
+      else:
+            attackRanged(getTargetX(), getTargetY())
+    elif start:
+        moveTo(12, 12)
+        if getX() == 12 and getY() == 12:
+            start = False
+    else:
+        turn(60)
 
